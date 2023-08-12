5e524ac11aef2d8a84e6692d74598becee9ddc94
Mona <monanom33@gmail.com>
2023-08-11 19:20:24 -0700

    5th

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - entry print
+*return - zero for success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
